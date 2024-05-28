c635bf35e0e8c248486e296ee91e6764fc432c34
pbukala-ct <piotr.bukala@commercetools.com>
2024-05-28 16:29:46 +1000

    Fix: Incremental update category keys

--- incremental-updater/src/clients/query.client.js
+++ incremental-updater/src/clients/query.client.js
@@ -1,17 +1,26 @@
 import { createApiRoot } from './create.client.js';
+import { apiRoot } from './create.client.js';
 
 export const productQueryArgs = {
-  staged: false,
-  expand: ['productSelection', 'taxCategory', 'productType', 'categories[*]'],
+  staged: false, expand: ['productSelection', 'taxCategory', 'productType', 'categories[*]']
 };
 
+const expandPaths = [
+  'productType',
+  'taxCategory',
+  'productSelection',
+  'categories[*]',
+];
+
 export async function getProductProjectionById(productId) {
-  return await createApiRoot()
+  console.log(apiRoot)
+  return await apiRoot
+    .withProjectKey({ projectKey:'retail-anz' })
     .productProjections()
     .withId({
       ID: Buffer.from(productId).toString(),
     })
-    .get({ productQueryArgs })
+    .get({ queryArgs: {staged: false, expand: expandPaths }})
     .execute()
     .then((response) => {
       return response.body;
